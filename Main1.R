e36d69c48f129f45436cb693c8f4781509da05b8
JanC99 <janschuller@gmx.ch>
2023-12-27 16:28:07 +0100

    new commit

--- Main1.R
+++ Main1.R
@@ -1,3 +1,6 @@
 # library(tidyverse)
+library(magrittr)
 MyPath="C:/Users/jansc/OneDrive/Documents/StefiHRV/data"
-list.files(path=MyPath)
+
+list.files(path=MyPath)[list.files(path=MyPath) %>% grep(".json",.)]
+
