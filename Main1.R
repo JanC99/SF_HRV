41299f883bda7f3b6646d208ccdb89ce05c4f7bc
JanC99 <janschuller@gmx.ch>
2023-12-27 17:20:32 +0100

    and a new commit

--- Main1.R
+++ Main1.R
@@ -2,3 +2,17 @@
 Myjason<-list.files(path=MyPath, full.names = T)[list.files(path=MyPath, full.names = T) 
                                                  %>% grep(".json",.)]
 
+
+test<-read_json(path=Myjason[2])
+
+#test %>% gather_array()
+
+test[[2]][[1]][[1]]$startTime
+ibi<-test[[2]][[1]][[1]]$rri %>% unlist()
+
+#test[[2]][[1]][[1]]$acceleration$series[[1]]
+
+
+move<-test[[2]][[1]][[1]]$acceleration$series %>% 
+  unlist() %>%
+  matrix(.,ncol=3, byrow = T)
--- Setup.R
+++ Setup.R
@@ -1,3 +1,9 @@
+install.packages("tidyjson")
+
 library(tidyverse)
 library(magrittr)
+library(tidyjson)
+
+
 MyPath="C:/Users/jansc/OneDrive/Documents/StefiHRV/data"
+
